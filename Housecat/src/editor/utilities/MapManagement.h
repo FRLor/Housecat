1db409fdc91c8f04ca3ea608cbfb3d0f96eebf74
FRLor <franzrey.lor@mymail.champlain.edu>
2024-03-24 01:34:21 -0600

    Feature: MapManagement -> load files Added: ProjectManagement barebones

---
+++ Housecat/src/editor/utilities/MapManagement.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <sol/sol.hpp>
+#include <filesystem>
+
+#include "SDLToolkit.h"
+
+#include "../../assetmanager/AssetManager.h"
+#include "../rendering/Canvas.h"
+
+class MapManagement {
+public:
+	MapManagement() = default;
+	~MapManagement() = default;
+
+	void LoadMap(const AssetManagerPtr& assetManager, const std::string& fileName);
+};
