9190d3bb9a0c605f48e439325b6831e44e704b24
FRLor <franzrey.lor@mymail.champlain.edu>
2024-03-24 00:10:59 -0600

    Feature: Editor -> FileDialogue

---
+++ Housecat/src/editor/utilities/FileDialogue.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+#include "FileDialogue.h"
+
+std::string FileDialogue::OpenFile(const char* filterPattern, HWND windowHandle) {
+	//dynamic allo for longer paths
+	std::vector<char> selectedFilePath(MAX_PATH);
+	//zero init the struct
+	OPENFILENAMEA openFileDialogStruct = { 0 };
+
+	ZeroMemory(&openFileDialogStruct, sizeof(openFileDialogStruct));
+
+	openFileDialogStruct.lStructSize = sizeof(openFileDialogStruct);
+	openFileDialogStruct.hwndOwner = windowHandle;
+	openFileDialogStruct.lpstrFilter = filterPattern;
+	//ptr to buffer's first element
+	openFileDialogStruct.lpstrFile = selectedFilePath.data();
+	openFileDialogStruct.nMaxFile = selectedFilePath.size();
+	openFileDialogStruct.Flags = OFN_NOCHANGEDIR | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	openFileDialogStruct.nFilterIndex = 1;
+
+	if (GetOpenFileNameA(&openFileDialogStruct)) {
+		//string from vector to ensure null-terminated
+		return openFileDialogStruct.lpstrFile;
+	}
+
+	return std::string();
+}
---
+++ Housecat/src/editor/utilities/FileDialogue.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <commdlg.h>
+
+class FileDialogue {
+public:
+	FileDialogue() = default;
+	~FileDialogue() = default;
+
+	static std::string OpenFile(const char* filterPattern = "Tilemap Files (*.map) | *.map\0*.map\0", HWND windowHandle = NULL);
+
+};
