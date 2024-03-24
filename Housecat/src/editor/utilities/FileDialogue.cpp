f3f24cb144e845edf90a2ca6ebda3a698e920dc4
FRLor <franzrey.lor@mymail.champlain.edu>
2024-03-24 00:12:05 -0600

    Updated: FileDialogue -> save method

--- Housecat/src/editor/utilities/FileDialogue.cpp
+++ Housecat/src/editor/utilities/FileDialogue.cpp
@@ -30,3 +30,23 @@ std::string FileDialogue::OpenFile(const char* filterPattern, HWND windowHandle)
 std::string FileDialogue::OpenTextureFile(const char* filterPattern, HWND windowHandle) {
 	return OpenFile(filterPattern, windowHandle);
 }
+
+std::string FileDialogue::SaveFile(const char* filterPath, HWND windowHandle) {
+	std::vector<char> filePath(MAX_PATH);
+	OPENFILENAMEA saveFileDialogStruct = { 0 };
+
+	saveFileDialogStruct.lStructSize = sizeof(saveFileDialogStruct);
+	saveFileDialogStruct.hwndOwner = windowHandle;
+	saveFileDialogStruct.lpstrFilter = filterPath;
+	saveFileDialogStruct.lpstrFile = filePath.data();
+	saveFileDialogStruct.nMaxFile = filePath.size();
+	saveFileDialogStruct.Flags = OFN_NOCHANGEDIR | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	saveFileDialogStruct.nFilterIndex = 1;
+	saveFileDialogStruct.lpstrDefExt = "lua";
+
+	if (GetSaveFileNameA(&saveFileDialogStruct)) {
+		return std::string(filePath.begin(), std::find(filePath.begin(), filePath.end(), '\0'));
+	}
+
+	return std::string();
+}
